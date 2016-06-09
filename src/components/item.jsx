ab3e839836fb901d230b91cfacfbc99d5ec27c51
BearFox <nick.mjac@gmail.com>
2016-06-09 23:42:35 +1200

    make more style changes to items

--- src/components/item.jsx
+++ src/components/item.jsx
@@ -1,4 +1,5 @@
-import React from "react";
+import React from "react"
+import MoreDetails from './more-details.jsx'
 
 console.log("item is loaded")
 
@@ -19,29 +20,31 @@ export default class Item extends React.Component {
     console.log('More DEEEEEEETAILS')
   }
 
+  noDetails() {
+    this.setState({clicked: false})
+  }
+
   render () {
     var itemDetails = this.props.itemDetails
 
     return (
       <div className="shop-item">
-        <img src={itemDetails.url} className='item-img'/>
-        <div className='item-deets'>
-          <div>NAME: <p>{itemDetails.name}</p></div>
-          <div>PRICE: <p>${ itemDetails.price }</p></div>
-          <div className='more-details'><p onClick={ this.handleMoredetails }>More details</p></div>
-          {
-            this.state.clicked ?
-              <div className='pony-details'>
-                <div>BREED: <p>{ itemDetails.breed }</p></div>
-                <div>AGE: <p>{ itemDetails.age }</p></div>
-                <p>{ itemDetails.description }</p>
-              </div>
-                : null
-            }
+        <div className='img-deets'>
+          <img src={itemDetails.url} className='item-img'/>
+          <div className='item-deets'>
+            <div>NAME: <p>{itemDetails.name}</p></div>
+            <div>PRICE: <p>${ itemDetails.price }</p></div>
+            <div className='more-details'><p onClick={ this.handleMoredetails }>More details</p></div>
+          </div>
         </div>
         <div>
-          <button onClick={this.handleBuy} >Buy</button>
+          <button onClick={this.handleBuy} >Add to cart</button>
         </div>
+          {
+            this.state.clicked ?
+              <MoreDetails details={ itemDetails } noDetails={ this.noDetails.bind(this) }/>
+                : null
+            }
       </div>
     )
    }
