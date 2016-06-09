3cd89c2cb868ec4f8e0f7784eb194fed76094d4b
hapekituarangi <hape.porter@gmail.com>
2016-06-09 12:20:26 +1200

    basic express server setup

--- index.js
+++ index.js
@@ -6,6 +6,11 @@ import http from 'http'
 const app = express()
 const server = http.createServer(app)
 
+app.get('/', (req, res) => {
+  console.log('GET /')
+  res.render('index')
+})
+
 server.listen(3000, () => {
   console.log('Server is listening on port 3000')
 })
